4bbaf49daafb6b46be5f9e6a7120c2546dff7f78
Jonas Almeida <jalmeida@mathbiol.org>
2014-11-21 16:29:35 -0600

    it works

--- README.md
+++ README.md
@@ -2,3 +2,42 @@
 =====
 
 Experimenting with [Sean's QUANAH](https://qmachine.github.io/quanah/), live experiments [here](https://jonasalmeida.github.io/experimentingWithQuanah).
+
+##1. lala
+Create a function, lala,
+
+		lala = function(v){
+			return 2*v
+		}
+
+and apply it to 2, as in *4 = 2.Q(lala)*
+
+		x = QUANAH.avar({val:2})
+		x.Q(function(evt){
+			this.val = lala(this.val); 
+			return evt.exit()
+		})
+		console.log(x) // x.val should be 4
+
+##2. lala after a while
+
+Run lala again, but only after something that takes a couple of seconds
+
+		x.Q(function (evt) {
+    		setTimeout(function () {
+        	x.val = lala(x.val);
+        	console.log(x.val)  // do 4 x 2 = 8 ...
+        	return evt.exit();
+    	}, 2000); // ... in 2 seconds
+    	return;
+		});
+
+		console.log(x.val)
+		x.Q(function(evt){
+    		this.val = lala(this.val+100); // while saying this right now 
+    		return evt.exit()
+		})
+		console.log(x.val) // it worked, we get (8 +100) x 2 --> 216  :-) !!
+								  //instead of ((4+100) x 2) x 2 which would have been 416
+
+##3. For here on best go straight to the [real fun](https://github.com/jonasalmeida/experimentingWithQuanah/blob/gh-pages/experimentingWithQuanah.js)
--- experimentingWithQuanah.js
+++ experimentingWithQuanah.js
@@ -1,1 +1,43 @@
 console.log('experimentingWithQuanah.js')
+
+// lala
+console.log('1. lala of 2')
+lala = function(v){
+    return 2*v
+}
+// create the variable
+x = QUANAH.avar({val:2})
+console.log('<-- created x')
+i = 0;
+console.log('at '+i+'/10 secs, x.val = '+x.val)
+t = setInterval(function(){i++;console.log('at '+i+'/10 secs, x.val = '+x.val)},100) // start logging x.val every 1/10 seconds
+setTimeout(function(){clearInterval(t)},3000) // for 3 minutes
+
+
+// 4 = 2.Q(lala)
+x.Q(function(evt){ this.val = lala(this.val); return evt.exit()})
+
+console.log('<-- doubled x ('+x.val+')')
+
+
+//console.log('2. lala of 2 after a while')
+
+//x.Q(function(evt){ var xx; this.val = lala(this.val); setTimeout(''+xx+'asdads;evt.exit,2000)})
+//console.log(x.val)
+
+
+x.Q(function (evt) {
+    setTimeout(function () {
+        x.val = lala(x.val);
+        console.log('<-- doubled again ('+x.val+')')
+        return evt.exit();
+    }, 2000);
+    return;
+});
+console.log('<-- promised to double it again in 2 mins x ('+x.val+')')
+//console.log(x.val)
+x.Q(function(evt){ this.val = lala(this.val+100); return evt.exit()})
+console.log('<-- added 100 and doubled it, which one ? :-) ('+x.val+')')
+console.log('current x:',x)
+//console.log(x.val)
+//console.log(x)
